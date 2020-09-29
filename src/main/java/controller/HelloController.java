faa6ad0bfc7db4dafdd7c0bc40e12d7538b15c88
guest.slab@gmail.com <guest.slab@gmail.com>
2020-09-29 16:43:03 +0900

    在新分支操作完毕提交3

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -14,8 +14,9 @@ public class HelloController {
 
     @RequestMapping(path = "/hello")
     public String Hello(){
-        System.out.println("编辑好了，我是原代码修改的内容");
+        System.out.println("编辑好了，我是第二次修改的内容");
         System.out.println("我是新增加的内容");
+        System.out.println("又增加了点东西");
         return "index";
     }
 }
